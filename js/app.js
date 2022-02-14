d90acf3be8fddee8ae5da7eaf981a61eff240e6a
Leily <leilymrz@gmail.com>
2022-02-14 13:23:09 -0800

    creating repository for porfolio and Resume

---
+++ js/app.js
@@ -0,0 +1,55 @@
+function swapStyleSheet(sheet) {
+    document.querySelector('#pagestyle').setAttribute('href', sheet);
+}
+
+function initiate() {
+    const printStyle = document.querySelector('#print-style-sheet');
+    const defaultStyle =document.querySelector('#default-style-sheet');
+
+    printStyle.onclick = function () {swapStyleSheet('css/print/print-view.css')}
+    defaultStyle.onclick = function () {swapStyleSheet('css/web/style.css')}
+}
+
+window.onload = initiate;
+
+
+// function toggleClass(span) {
+//     span.className ='icon-sun-o'
+// }
+
+// const $light = document.querySelector('#Light');
+// const $dark = document.querySelector('#Dark');
+
+// const toggleBtn = document.querySelector('#toggleBtn')
+
+// const toggleBtn = document.querySelector('#toggle-btn');
+
+
+
+
+
+
+// $light.addEventListener('click', lightTheme);
+// $dark.addEventListener('click', darkTheme);
+
+// function lightTheme() {
+//     document.body.classList.toggle('light');
+//     $dark.hidden = false
+//     $light.hidden = true
+// }
+
+// function darkTheme() {
+//     document.body.classList.toggle('dark');
+    // $dark.hidden = true
+    // $light.hidden = false
+// }
+
+
+
+// import "./styles.css";
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//   document.body.classList.toggle('dark');
+// });
