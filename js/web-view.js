ed1ab36240061b628938c2cda7360634ef1961e5
Leily <leilymrz@gmail.com>
2022-02-19 11:47:45 -0800

    working on Javascript

--- js/web-view.js
+++ js/web-view.js
@@ -1,9 +1,23 @@
 const barsToggle = document.querySelector('#bars')
 const nav = document.querySelector('.navbar')
+const barsIcon = document.querySelector('.icon-bars')
 
-barsToggle.addEventListener('click', () => {
-  nav.classList.toggle('nav--visible')
-})
+let showMenu = false
+
+barsToggle.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    nav.classList.toggle('nav--visible')
+    barsToggle.classList.add('icon-close')
+    barsToggle.classList.remove('icon-bars')
+    showMenu = true
+  } else {
+    nav.classList.remove('nav--visible')
+    barsToggle.classList.add('icon-bars')
+    showMenu = false
+  }
+}
 
 const settingBtn = document.querySelector('#icon-setting')
 const settingList = document.querySelector('.settings-list')
